895efcad53eece104e2779147c4451f0e990515d
Kacper Duda <lollel60606@gmail.com>
2024-03-12 05:59:49 +0100

    trojkat pascala

--- cw3/zad1.c
+++ cw3/zad1.c
@@ -1,13 +1,40 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void trojkatPacsala(int n) {
-	for (int i = 0; i < n; i++) {
+void printTriangle(int** arr, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
 
-	}
+void trojkatPascala(int n) {
+    // Alokuje pamiec dla wskaznikow do kazdego wiersza
+    int** arr = (int**)malloc(n * sizeof(int*));
+    for (int i = 0; i < n; i++) {
+        // Alokuje pamiec dla kazdego wiersza
+        arr[i] = (int*)malloc((i + 1) * sizeof(int));
+        arr[i][0] = 1; // Pierwsza wartosc w kazdym wierszu to zawsze 1
+        arr[i][i] = 1; // Ostatnia wartosc w kazdym wierszu tez jest 1
+
+        // Uzupelniamy srodkowe wartosci wiersza, jesci istnieja
+        for (int j = 1; j < i; j++) {
+            arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+        }
+    }
+
+    printTriangle(arr, n);
+
+    // Zwolnienie pamieci
+    for (int i = 0; i < n; i++) {
+        free(arr[i]);
+    }
+    free(arr);
 }
 
 int main() {
-
-	return 0;
+    trojkatPascala(5);
+    return 0;
 }
