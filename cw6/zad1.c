bd92d4e99e8228e5e4437568947a616615011f09
Kacper Duda <lollel60606@gmail.com>
2024-04-09 08:46:45 +0200

    zad1

--- cw6/zad1.c
+++ cw6/zad1.c
@@ -1,7 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main() {
+union dane {
+	int calkowita;
+	float zmiennoPrzecinkowa;
+	char znak;
+};
 
+int main() {
+	union dane d1;
+	d1.calkowita = 44;
+	printf("liczba calkowita w union: %d\n", d1.calkowita);
+	d1.zmiennoPrzecinkowa = 3.14;
+	printf("liczba float w union: %f\n", d1.zmiennoPrzecinkowa);
+	d1.znak = 'A';
+	printf("znak w union: %c\n", d1.znak);
 	return 0;
 }
