9ba4832e7342ed42a79a55bcc4c0e27cf897172a
Kacper Duda <lollel60606@gmail.com>
2024-04-09 09:04:08 +0200

    zad2

--- cw6/zad2.c
+++ cw6/zad2.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 
 int main() {
-
+	int tab[10] = { 0,1,2,3,4,5,6,7,8,9 };
+	int *tabPointer = &tab[0];
+	for (int i = 0; i < sizeof(tab)/sizeof(int); i++) {
+		printf("%d\n", *tabPointer);
+		tabPointer++;
+	}
 	return 0;
 }
