71017e08df21b35fe22be1d304f2bc4e177f1478
Kacper Duda <lollel60606@gmail.com>
2024-04-09 09:33:00 +0200

    zad3

--- cw6/zad3.c
+++ cw6/zad3.c
@@ -1,7 +1,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main() {
+int dodawanie(int x, int y) {
+	return x + y;
+}
 
+int odejmowanie(int x, int y) {
+	return x - y;
+}
+
+int mnozenie(int x, int y) {
+	return x * y;
+}
+
+int main() {
+	int (*wskaznikFunkcji)(int, int);
+	int x, y, operacja;
+	printf("\nWybierz operacje:\n\t1 - Dodawanie\n\t2 - Odejmowanie\n\t3 - Mnozenie\n");
+	scanf("%d", &operacja);
+	switch (operacja){
+	case 1:
+		wskaznikFunkcji = dodawanie;
+		break;
+	case 2:
+		wskaznikFunkcji = odejmowanie;
+		break;
+	case 3:
+		wskaznikFunkcji = mnozenie;
+		break;
+	default:
+		break;
+	}
+	printf("Podaj 2 liczby: \n");
+	scanf("%d %d", &x, &y);
+	printf("\nWynik: %d", wskaznikFunkcji(x, y));
 	return 0;
 }
