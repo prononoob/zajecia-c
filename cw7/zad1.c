9125560b03d693460d04c52f5f6c7c7b54a2c584
Kacper Duda <lollel60606@gmail.com>
2024-04-19 13:57:45 +0200

    czesciowo dziala czesc druga w 1 zad ale nie do konca

--- cw7/zad1.c
+++ cw7/zad1.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 int getFirstAndLastDigit(char* line) {
     bool foundFirstDigit = false;
-    int firstDigit = 0, secondDigit = 0;
+    int firstDigit = 0, secondDigit = -1;
     while (*line != '\0') {
         if (*line >= '0' && *line <= '9' && !foundFirstDigit) {
             firstDigit = *line - '0';
@@ -14,32 +15,51 @@ int getFirstAndLastDigit(char* line) {
         else if (*line >= '0' && *line <= '9' && foundFirstDigit) {
             secondDigit = *line - '0';
         }
-        // here code but dont write it pls
         else if (*line == 'o' || *line == 't' || *line == 'f' || *line == 's' || *line == 'e' || *line == 'n' || *line == 'z') {
+            //printf("\tFD%d\tSD: %d\n", firstDigit, secondDigit);
             secondDigit = findDigits(&line, secondDigit);
+            if (secondDigit >= 0 && !foundFirstDigit) {
+                firstDigit = secondDigit;
+                foundFirstDigit = true;
+            }
+            //printf("\tFD%d\tSD: %d\n", firstDigit, secondDigit);
         }
 
+        //printf("FD%d\tSD: %d\n", firstDigit, secondDigit);
         line++;
 
     }
-    printf("%d%d\n", firstDigit, secondDigit);
+    //printf("%d\t%d\n", firstDigit, secondDigit);
     int result = firstDigit * 10 + secondDigit;
-    printf("%d\n", result);
+    printf("WYNIK:\t%d\n", result);
     return result;
 }
 
 bool compareDigitInString(char** linePtr, char* strToCheck) {
-    bool isFound = false;
-    
-    return isFound;
-}
+    int strLength = strlen(strToCheck);
+    int i = 0;
+    while (i < strLength) {
+        //printf("%c %c\n", (**linePtr), strToCheck[i]);
+        if ((**linePtr) != strToCheck[i]) {
+            (*linePtr)--;
+            return false;
+        }
+        (*linePtr)++;
+        i++;
+    }
+    (*linePtr)--;
 
+    return true;
+}
 
 int findDigits(char** linePtr, int defaultVal) {
     int result = defaultVal;
     switch (**linePtr) {
     case 'o':
-        compareDigitInString(linePtr, "ne");
+        (*linePtr)++;
+        if (compareDigitInString(linePtr, "ne")) {
+            result = 1;
+        }
         break;
     case 't':
         // Code for case 2
@@ -50,29 +70,32 @@ int findDigits(char** linePtr, int defaultVal) {
         else if (**linePtr == 'h') {
             compareDigitInString(linePtr, "hree");
         }
+        else {
+            (*linePtr)--;
+        }
         break;
     case 'f':
         // Code for case 3
-        printf("You chose option 3\n");
+        //printf("You chose option 3\n");
         break;
     case 's':
         // Code for case 4
-        printf("You chose option 4\n");
+        //printf("You chose option 4\n");
         break;
     case 'e':
         // Code for case 5
-        printf("You chose option 5\n");
+        //printf("You chose option 5\n");
         break;
     case 'n':
         // Code for case 6
-        printf("You chose option 6\n");
+        //printf("You chose option 6\n");
         break;
     case 'z':
         // Code for case 7
-        printf("You chose option 7\n");
+        //printf("You chose option 7\n");
         break;
-        return result;
     }
+    return result;
 }
 
 int main() {
@@ -97,6 +120,6 @@ int main() {
     }
 
     fclose(file);
-    printf("%d\n", result);
+    printf("WYNIK KONCOWY \t%d\n", result);
 	return 0;
 }
